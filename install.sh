fa545936245eb11e46c5d26a8df35e3dc05d5720
Yifan Chen <cyifan@sdfiana008.sdf.slac.stanford.edu>
2023-11-14 14:41:27 -0800

    update the install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "initiating submodules"
 git submodule init
-git submodule update --remote
+git submodule update #--remote
 
 echo "installing larnd-sim"
 cd modules/larnd-sim
-git checkout 1b8d97bf9ccdbee56ded4ceb4582ab3570c32d3a
+#git checkout 1b8d97bf9ccdbee56ded4ceb4582ab3570c32d3a
 export SKIP_CUPY_INSTALL=1
 pip install . --user
 if [ $? -gt 0 ]
@@ -17,7 +17,7 @@ cd -
 
 echo "installing event parser"
 cd modules/larpix_readout_parser
-git checkout 6f6fad0c0977dc4b420770d81d5647d2345ed828
+#git checkout 6f6fad0c0977dc4b420770d81d5647d2345ed828
 pip install . --user
 if [ $? -gt 0 ]
 then
@@ -28,7 +28,7 @@ cd -
 
 echo "installing SuperaAtomic"
 cd modules/SuperaAtomic
-git checkout bde4a158ff31034a6381c37c81b13fe9c0feb2b6
+#git checkout bde4a158ff31034a6381c37c81b13fe9c0feb2b6
 export SUPERA_WITHOUT_PYTHON=1
 pip install . --user
 if [ $? -gt 0 ]
@@ -40,7 +40,7 @@ cd -
 
 echo "installing edep2supera"
 cd modules/edep2supera
-git checkout 296ae904d04ccf9e69f16c8087b56c8406d0d994
+#git checkout 296ae904d04ccf9e69f16c8087b56c8406d0d994
 pip install . --user
 if [ $? -gt 0 ]
 then
@@ -51,7 +51,7 @@ cd -
 
 echo "installing larnd2supera"
 cd modules/larnd2supera
-git checkout a86d512a6ab83f563655d4dfdef3cca92c606dc9
+#git checkout a86d512a6ab83f563655d4dfdef3cca92c606dc9
 pip install . --user
 if [ $? -gt 0 ]
 then
